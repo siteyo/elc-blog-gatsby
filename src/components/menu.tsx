626706a1917b235fa028426086863569409ca143
Saito Yuki <phicyanoc@gmail.com>
2021-08-13 21:24:57 +0900

    Menuコンポーネントを追加

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 
 const Menu: React.VFC = () => {
-  return <h1>Menu</h1>;
+  const data = useStaticQuery<GatsbyTypes.MenuLinksQuery>(
+    graphql`
+      query MenuLinks {
+        site {
+          siteMetadata {
+            menuLinks {
+              link
+              name
+            }
+          }
+        }
+      }
+    `,
+  );
+  return (
+    <Grid container justifyContent="center">
+      {data.site?.siteMetadata?.menuLinks?.map(menu => (
+        <Grid item xs={2}>
+          <Link to={menu?.link ?? '/'}>
+            <Typography variant="h5" color="primary">
+              {menu?.name}
+            </Typography>
+          </Link>
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default Menu;
