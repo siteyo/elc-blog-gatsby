565e70507ffc1835642d070f7e97a94efc20606a
Saito Yuki <phicyanoc@gmail.com>
2021-08-30 23:47:40 +0900

    WorksページのPaginationコンポーネントを調整

--- src/templates/works.tsx
+++ src/templates/works.tsx
@@ -3,6 +3,16 @@ import { graphql, PageProps, navigate } from 'gatsby';
 import { Pagination } from '@material-ui/lab';
 import Layout from 'components/layout';
 import PostCard from 'components/PostCard';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  pageRoot: {
+    margin: '1rem 0',
+  },
+  pageButtons: {
+    justifyContent: 'center',
+  },
+});
 
 export interface WorksPageContext {
   limit: number;
@@ -14,6 +24,7 @@ export interface WorksPageContext {
 const Works: React.VFC<
   PageProps<GatsbyTypes.WorksPageQuery, WorksPageContext>
 > = ({ data, pageContext }) => {
+  const classes = useStyles();
   const content = data.allContentfulWorks.edges.map<React.ReactChild>(edge => (
     <PostCard
       key={edge.node.slug}
@@ -33,8 +44,10 @@ const Works: React.VFC<
     <Layout>
       <div>{content}</div>
       <Pagination
+        classes={{ root: classes.pageRoot, ul: classes.pageButtons }}
         page={pageContext.currentPage}
         count={pageContext.numPage}
+        color="secondary"
         onChange={handleChange}
       />
     </Layout>
