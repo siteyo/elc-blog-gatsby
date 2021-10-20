aebb06c4b3f3e862a31c042e05938ee2d7e673ed
Saito Yuki <phicyanoc@gmail.com>
2021-10-20 23:01:59 +0900

    FooterのSNSボタンを削除

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,6 @@ import {
   makeStyles,
 } from '@material-ui/core';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import Sns from 'components/Sns';
 
 /* Styles */
 const useStyles = makeStyles({
@@ -27,17 +26,13 @@ const Footer: React.VFC = () => {
     <>
       <Divider className={classes.divider} />
       <Grid container justifyContent="space-between">
-        <Grid item sm={2} xs={6} />
         <Hidden xsDown>
-          <Grid item sm={8} style={{ textAlign: 'center' }}>
+          <Grid item sm={12} style={{ textAlign: 'center' }}>
             <IconButton onClick={handleClick}>
               <ArrowDropUpIcon />
             </IconButton>
           </Grid>
         </Hidden>
-        <Grid item sm={2} xs={6}>
-          <Sns />
-        </Grid>
         <Grid item sm={12}>
           <Typography variant="body2" color="secondary">
             Copyright © elc, All rights reserved.
